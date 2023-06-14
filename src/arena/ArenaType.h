34ba3a493b53e110e2dae0ed86866af11089807b
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-06-14 23:44:01 +0100

    Using namespace arena for generated enums

--- src/arena/ArenaType.h
+++ src/arena/ArenaType.h
@@ -6,45 +6,47 @@
 #include <string>
 #include <unordered_map>
 
-class ArenaType
-{
-public:
-	enum Value
+namespace arena {
+	class ArenaType
 	{
-		Training,
-	};
-
-	ArenaType() = default;
-	constexpr ArenaType(Value value) : value(value) { }
-
-	constexpr operator Value() const { return value; }
-	explicit operator bool() const = delete;
-	constexpr bool operator==(ArenaType a) const { return value == a.value; }
-	constexpr bool operator!=(ArenaType a) const { return value != a.value; }
-
-	std::string to_string() const
-	{
-		switch (value)
+	public:
+		enum Value
 		{
-		case ArenaType::Training: return "Training";
-		default: return "Unknown";
-		}
-	}
-
-	static Value from_string(const std::string& name)
-	{
-		static const std::unordered_map<std::string, Value> value_map = {
-			{ "Training", ArenaType::Training },
+			Training,
 		};
 
-		auto it = value_map.find(name);
-		if (it != value_map.end())
-			return it->second;
-		return ArenaType::Training; // Return default value if no match is found
-	}
+		ArenaType() = default;
+		constexpr ArenaType(Value value) : value(value) { }
 
-private:
-	Value value;
-};
+		constexpr operator Value() const { return value; }
+		explicit operator bool() const = delete;
+		constexpr bool operator==(ArenaType a) const { return value == a.value; }
+		constexpr bool operator!=(ArenaType a) const { return value != a.value; }
+
+		std::string to_string() const
+		{
+			switch (value)
+			{
+			case ArenaType::Training: return "Training";
+			default: return "Unknown";
+			}
+		}
+
+		static Value from_string(const std::string& name)
+		{
+			static const std::unordered_map<std::string, Value> value_map = {
+				{ "Training", ArenaType::Training },
+			};
+
+			auto it = value_map.find(name);
+			if (it != value_map.end())
+				return it->second;
+			return ArenaType::Training; // Return default value if no match is found
+		}
+
+	private:
+		Value value;
+	};
+}
 
 #endif
