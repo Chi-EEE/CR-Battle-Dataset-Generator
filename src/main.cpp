ee2502ec44e7cae9e529e73b0e2d5ea5651f92f1
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-07-05 21:08:49 +0100

    Able to go through inputted files

--- src/main.cpp
+++ src/main.cpp
@@ -28,25 +28,31 @@ using namespace std;
 using namespace sc;
 
 int main(int argc, char* argv[]) {
-    if (argc != 1) {
-        cout << "Failed to get filename";
-    }
+    // The first argument (argv[0]) is the program name
+    // The following arguments (argv[1] onwards) are the file paths
 
-    SupercellSWF swf;
-
-    try {
-        swf.load(argv[0]);
+    // Check if any file paths were provided
+    if (argc > 1) {
+        // Iterate over the file paths
+        for (int i = 1; i < argc; ++i) {
+            SupercellSWF swf;
+            try {
+                swf.load(argv[i]);
+            }
+            catch (const std::exception& err) {
+                cout << "Failed to load file. Error: " << err.what() << endl;
+            }
+            cout << "File has: " << endl;
+            cout << swf.exports.size() << " export names" << endl;
+            cout << swf.textures.size() << " textures" << endl;
+            cout << swf.textFields.size() << " textfields" << endl;
+            cout << swf.shapes.size() << " shapes" << endl;
+            cout << swf.movieClips.size() << " movieclips" << endl;
+        }
     }
-    catch (const std::exception& err) {
-        cout << "Failed to load file. Error: " << err.what() << endl;
+    else {
+        std::cout << "No file paths provided." << std::endl;
     }
-
-    cout << "File has: " << endl;
-    cout << swf.exports.size() << " export names" << endl;
-    cout << swf.textures.size() << " textures" << endl;
-    cout << swf.textFields.size() << " textfields" << endl;
-    cout << swf.shapes.size() << " shapes" << endl;
-    cout << swf.movieClips.size() << " movieclips" << endl;
 
     return 0;
 }
