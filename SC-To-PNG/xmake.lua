efa3873adf4f51b8e3b198f5122a2e21bb180879
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-07-28 20:22:44 +0100

    Updated the SupercellFlash files

--- SC-To-PNG/xmake.lua
+++ SC-To-PNG/xmake.lua
@@ -1,15 +1,29 @@
 add_rules("mode.debug", "mode.release")
+set_runtimes("MDd")
 add_requires("libpng")
 
 set_languages("cxx17")
 
 target("SC-To-PNG")
     set_kind("binary")
+
     add_packages("libpng")
     add_headerfiles("src/**.h")
     add_files("src/**.cpp")
     
     add_includedirs("include", { public = true })
+    
+    add_ldflags("/ignore:4099")
 
-    add_links("SupercellFlash", "SupercellCompression", "SupercellCompression", "LZMA", "LZHAM", "Zstandard")
     add_linkdirs("lib/$(mode)")
+
+    add_links(
+        "ASTC",
+        "ETCPACK",
+        "LZHAM", 
+        "LZMA", 
+        "SupercellFlash", 
+        "SupercellCompression", 
+        "SupercellTextureLoader", 
+        "Zstandard"
+        )
