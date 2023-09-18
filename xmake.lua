45f8436fcdb620fcf6e0ec47f556883229425f8f
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-09-18 17:50:49 +0100

    Try skia 93

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,7 @@
 add_rules("mode.debug", "mode.release")
-add_requires("skia", "opencv")
+
+add_repositories("repo repository")
+add_requires("skia 93", "opencv")
 add_requires("cpptrace", "fmt", "tl_expected", "spdlog")
 add_requires("nlohmann_json", "toml++")
 
@@ -8,7 +10,7 @@ set_languages("cxx17")
 target("CR-Battle-Dataset-Generator")
     set_kind("binary")
 
-    add_packages("skia", "opencv")
+    add_packages("skia 93", "opencv")
     add_packages("cpptrace", "fmt", "tl_expected", "spdlog")
     add_packages("nlohmann_json", "toml++")
    
