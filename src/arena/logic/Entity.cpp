dc194e3d8b53d0d1e3ccd9697e9f0436e98c275c
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-09-29 19:58:09 +0100

    Using reference to settings_json

--- src/arena/logic/Entity.cpp
+++ src/arena/logic/Entity.cpp
@@ -108,7 +108,8 @@ namespace arena::logic {
 		if (!this->level_ui) {
 			return;
 		}
-		std::filesystem::path asset_directory(Global::getSettings()["asset_directory"].get<std::string>());
+		const json& settings_json = Global::getSettings();
+		std::filesystem::path asset_directory(settings_json["asset_directory"].get<std::string>());
 		ImageLoader& image_loader = ImageLoader::get_instance();
 		auto level_ui = image_loader.try_load_image(asset_directory / "sprites" / "ui" / fmt::format("{}_level.png", this->is_blue ? "player" : "enemy")).value();
 
