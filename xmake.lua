ec9eaf36fe86b0688be6693fe6fa55606ce827d6
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-09-29 19:56:22 +0100

    Using proper 'json-schema-validator'

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,8 @@
 add_rules("mode.debug", "mode.release")
 
-add_repositories("my-repo repository")
-
 add_requires("skia", "opencv")
 add_requires("cpptrace", "fmt", "tl_expected", "spdlog")
-add_requires("json-schema-validatora", "toml++")
+add_requires("json-schema-validator", "toml++")
 
 set_languages("cxx17")
 
@@ -13,7 +11,7 @@ target("CR-Battle-Dataset-Generator")
 
     add_packages("skia", "opencv")
     add_packages("cpptrace", "fmt", "tl_expected", "spdlog")
-    add_packages("json-schema-validatora", "toml++")
+    add_packages("json-schema-validator", "toml++")
    
     add_files("src/**.cpp")
     add_headerfiles("src/**.h", "src/**.hpp")
